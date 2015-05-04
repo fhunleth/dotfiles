4428583801ec7371fc0b1703dce210c7c90f3661
Frank Hunleth <fhunleth@troodon-software.com>
2015-05-04 13:31:36 -0400

    Some of my updates

--- setup.sh
+++ setup.sh
@@ -7,23 +7,23 @@ ln -sf `pwd`/vim/ftplugin/ruby.vim ~/.vim/ftplugin/ruby.vim
 ln -sf `pwd`/vim/ftplugin/erlang.vim ~/.vim/ftplugin/erlang.vim
 
 # bash
-mkdir -p ~/.bash_files
-ln -sf `pwd`/bash/aliases ~/.bash_files/aliases
-ln -sf `pwd`/bash/prompts ~/.bash_files/prompts
+#mkdir -p ~/.bash_files
+#ln -sf `pwd`/bash/aliases ~/.bash_files/aliases
+#ln -sf `pwd`/bash/prompts ~/.bash_files/prompts
 
 # ack
-ln -sf `pwd`/ack/ackrc ~/.ackrc
+#ln -sf `pwd`/ack/ackrc ~/.ackrc
 
 # map caps to esc
-ln -sf `pwd`/xmodmap/xmodmap-esc ~/.xmodmap-esc
+#ln -sf `pwd`/xmodmap/xmodmap-esc ~/.xmodmap-esc
 
 # tmux
-ln -sf `pwd`/tmux/tmux.conf ~/.tmux.conf
-ln -sf `pwd`/tmux/teamocil ~/.teamocil
+#ln -sf `pwd`/tmux/tmux.conf ~/.tmux.conf
+#ln -sf `pwd`/tmux/teamocil ~/.teamocil
 
 # synergy
-ln -sf `pwd`/synergy/synergy.conf ~/.synergy.conf
+#ln -sf `pwd`/synergy/synergy.conf ~/.synergy.conf
 
 # bins
-ln -sf `pwd`/bin/tspace ~/bin/tspace
-ln -sf `pwd`/bin/list-terminal-colors ~/bin/list-terminal-colors
+#ln -sf `pwd`/bin/tspace ~/bin/tspace
+#ln -sf `pwd`/bin/list-terminal-colors ~/bin/list-terminal-colors
