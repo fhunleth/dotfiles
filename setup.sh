347097d5509318db32e19477bfab304fe81bb707
Frank Hunleth <fhunleth@troodon-software.com>
2018-01-02 20:18:44 -0500

    Add vscode and update a couple other things

--- setup.sh
+++ setup.sh
@@ -15,7 +15,7 @@ ln -sf `pwd`/vim/ftplugin/erlang.vim ~/.vim/ftplugin/erlang.vim
 #ln -sf `pwd`/ack/ackrc ~/.ackrc
 
 # map caps to esc
-#ln -sf `pwd`/xmodmap/xmodmap-esc ~/.xmodmap-esc
+ln -sf `pwd`/xmodmap/xmodmap-esc ~/.xmodmap-esc
 
 # tmux
 #ln -sf `pwd`/tmux/tmux.conf ~/.tmux.conf
@@ -27,3 +27,8 @@ ln -sf `pwd`/vim/ftplugin/erlang.vim ~/.vim/ftplugin/erlang.vim
 # bins
 #ln -sf `pwd`/bin/tspace ~/bin/tspace
 #ln -sf `pwd`/bin/list-terminal-colors ~/bin/list-terminal-colors
+
+# vscode
+mkdir -p ~/.config/Code/User/
+ln -sf `pwd`/vscode/settings.json ~/.config/Code/User/settings.json
+ln -sf `pwd`/vscode/keybindings.json ~/.config/Code/User/keybindings.json
